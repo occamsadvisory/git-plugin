7137a33bc77956940dbeb3d818f041421f566ed5
Rahul Raut <10260232+rahulmr@users.noreply.github.com>
2023-08-10 16:51:06 +0000

    commit version

--- git-plugin.php
+++ git-plugin.php
@@ -16,8 +16,8 @@
  * Plugin Name:       Git Plugin
  * Plugin URI:        https://github.com/occamsadvisory/git-plugin
  * Description:       For testing git and DevOps features
- * Version:           1.0.4
- * Author:            Vinay B
+ * Version:           1.0.5
+ * Author:            Vinay Bisht
  * Author URI:        https://github.com/rahulmr
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'GIT_PLUGIN_VERSION', '1.0.4' );
+define( 'GIT_PLUGIN_VERSION', '1.0.5' );
 
 /**
  * The code that runs during plugin activation.
